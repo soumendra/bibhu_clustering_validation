49e67ab9d9a142a674ecb2f10f30fa5a4f0c6def
Soumendra Dhanee <soumendra@gmail.com>
2021-02-18 00:52:11 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,48 @@
 import streamlit as st
 import pandas as pd
 
+st.markdown(
+    """
+<style>
+.reportview-container .markdown-text-container {
+    font-family: monospace;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+.Widget>label {
+    color: white;
+    font-family: monospace;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: monospace;
+}
+.st-bb {
+    background-color: transparent;
+}
+.st-at {
+    background-color: #0c0080;
+}
+footer {
+    font-family: monospace;
+}
+.reportview-container .main footer, .reportview-container .main footer a {
+    color: #0c0080;
+}
+header .decoration {
+    background-image: none;
+}
+
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+
+
 
 st.sidebar.markdown("# automl v1")
 
